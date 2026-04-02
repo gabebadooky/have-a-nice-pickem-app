2e2b30e9249f72453b4a2c459fcf49158eae9cd6
Gabriel Baduqui <baduquig@gmail.com>
2026-04-01 20:45:36 -0700

    Retrieve BoxScores entity for a given game and team

--- src/main/java/com/haveanicepickem/restservice/service/CompetitorService.java
+++ src/main/java/com/haveanicepickem/restservice/service/CompetitorService.java
@@ -1,5 +1,6 @@
 package com.haveanicepickem.restservice.service;
 
+import java.lang.foreign.Linker.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,15 +8,27 @@ import org.springframework.stereotype.Service;
 
 import com.haveanicepickem.restservice.dto.CompetitorResponseDTO;
 import com.haveanicepickem.restservice.entity.BettingOddsEntity;
+import com.haveanicepickem.restservice.entity.BoxScoresEntity;
+import com.haveanicepickem.restservice.entity.BoxScoresId;
+import com.haveanicepickem.restservice.entity.StatEntity;
 import com.haveanicepickem.restservice.repository.BettingOddsRepository;
+import com.haveanicepickem.restservice.repository.BoxScoresRepository;
+import com.haveanicepickem.restservice.repository.StatRepository;
 
 @Service
 public class CompetitorService {
 
     private BettingOddsRepository bettingOddsRepository;
+    private BoxScoresRepository boxScoresRepository;
+    private BoxScoresId boxScoresID;
 
     public CompetitorResponseDTO getCompetitor(String gameID, String teamID) {
+        int boxscoreIdentifier = new BoxScoresId(gameID, teamID).hashCode();
+
         Optional<List<BettingOddsEntity>> bettingOdds = bettingOddsRepository.findByGameIdAndTeamId(gameID, teamID);
+        Optional<BoxScoresEntity> boxscore = boxScoresRepository.findById(boxScoresID);
+
+
     }
 
 }
