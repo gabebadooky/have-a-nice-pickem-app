9b0ac112499aa2484d7a866269ca055682bb4131
Gabriel Baduqui <baduquig@gmail.com>
2026-04-01 20:52:23 -0700

    Retrieve Team entity for a given teamID

--- src/main/java/com/haveanicepickem/restservice/service/CompetitorService.java
+++ src/main/java/com/haveanicepickem/restservice/service/CompetitorService.java
@@ -11,9 +11,11 @@ import com.haveanicepickem.restservice.entity.BettingOddsEntity;
 import com.haveanicepickem.restservice.entity.BoxScoresEntity;
 import com.haveanicepickem.restservice.entity.BoxScoresId;
 import com.haveanicepickem.restservice.entity.StatEntity;
+import com.haveanicepickem.restservice.entity.TeamEntity;
 import com.haveanicepickem.restservice.repository.BettingOddsRepository;
 import com.haveanicepickem.restservice.repository.BoxScoresRepository;
 import com.haveanicepickem.restservice.repository.StatRepository;
+import com.haveanicepickem.restservice.repository.TeamRepository;
 
 @Service
 public class CompetitorService {
@@ -22,13 +24,17 @@ public class CompetitorService {
     private BoxScoresRepository boxScoresRepository;
     private BoxScoresId boxScoresID;
     private StatRepository statRepository;
+    private TeamRepository teamRepository;
 
     public CompetitorResponseDTO getCompetitor(String gameID, String teamID) {
         int boxscoreIdentifier = new BoxScoresId(gameID, teamID).hashCode();
 
         Optional<List<BettingOddsEntity>> bettingOdds = bettingOddsRepository.findByGameIdAndTeamId(gameID, teamID);
         Optional<BoxScoresEntity> boxscore = boxScoresRepository.findById(boxScoresID);
-        Optional<List<StatEntity>> stats = statRepository.findByGameIdAndTeamId(gameID, teamID);
+        Optional<List<StatEntity>> gameStats = statRepository.findByGameIdAndTeamId(gameID, teamID);
+        Optional<List<StatEntity>> teamStats = statRepository.findByTeamId(teamID);
+        Optional<TeamEntity> team = teamRepository.findById(teamID);
+
     }
 
 }
