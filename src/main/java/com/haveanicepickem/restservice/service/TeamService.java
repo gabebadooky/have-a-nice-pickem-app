4313c10d270f02c75a63e37336ad419fe82f762c
Gabriel Baduqui <baduquig@gmail.com>
2026-04-05 08:34:33 -0700

    Extrapolate TeamRecord services

--- src/main/java/com/haveanicepickem/restservice/service/TeamService.java
+++ src/main/java/com/haveanicepickem/restservice/service/TeamService.java
@@ -1,9 +1,12 @@
 package com.haveanicepickem.restservice.service;
 
+import org.springframework.stereotype.Service;
+
 import com.haveanicepickem.restservice.dto.TeamMapper;
 import com.haveanicepickem.restservice.dto.TeamResponseDTO;
 import com.haveanicepickem.restservice.repository.TeamRepository;
 
+@Service
 public class TeamService {
 
     private TeamRepository teamRepository;
